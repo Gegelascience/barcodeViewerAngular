9bdf5d01cb8956142c34d6d6f2eafa50dde94e37
VERCHERE Rémi <remi.verchere2@gmail.com>
2023-07-25 16:09:07 +0200

    save barcode as files

--- src/app/components/barcode-renderer/barcode-renderer.component.ts
+++ src/app/components/barcode-renderer/barcode-renderer.component.ts
@@ -81,6 +81,67 @@ export class BarcodeRendererComponent implements AfterViewInit {
     ctx.moveTo(begin[0],begin[1]);
     ctx.lineTo(end[0],end[1]);
     ctx.stroke();
-}
+  }
+
+  savePng() {
+    const pngUrl =this.barcodeCanvas.nativeElement.toDataURL();
+    const link = document.createElement("a");
+    link.href = pngUrl;
+    link.download = "barcode.png";
+    link.click();
+  }
+
+  saveSvg() {
+    const myEan = new EanWrapper(this.possibleEan);
+    const svgEl = this.generateSvgElement(myEan);
+
+    var serializer = new XMLSerializer();
+    var source = serializer.serializeToString(svgEl);
+
+    if(!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)){
+        source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
+    }
+
+    source = '<?xml version="1.0" encoding="utf-8"?>\r\n' + source;
+
+    //convert svg source to URI data scheme.
+    var svgurl = "data:image/svg+xml;charset=utf-8,"+encodeURIComponent(source);
+
+    const link = document.createElement("a");
+    link.href = svgurl;
+    link.download = "barcode.svg";
+    link.click();
+  }
+
+  generateSvgElement(eanW: EanWrapper) {
+    const barcodeValue = eanW.barcodeValue;
+    const svgContainer = document.createElementNS("http://www.w3.org/2000/svg",'svg')
+    svgContainer.setAttribute("version","1.1")
+    svgContainer.setAttribute("baseProfile","full")
+    svgContainer.setAttribute("width","700")
+    svgContainer.setAttribute("height","200")
+    
+
+    const gContainer = document.createElementNS('http://www.w3.org/2000/svg','g');
+    gContainer.setAttribute("stroke","black")
+    let index = 10;
+    for (let indexB = 0; indexB < barcodeValue.length; indexB++) {
+        if (barcodeValue[indexB] == "1"){
+            const lineContainer = document.createElementNS('http://www.w3.org/2000/svg','line');
+            lineContainer.setAttribute("stroke-width","4")
+            lineContainer.setAttribute("y1","10")
+            lineContainer.setAttribute("x1",index.toString())
+            lineContainer.setAttribute("y2","50")
+            lineContainer.setAttribute("x2",index.toString())
+            gContainer.appendChild(lineContainer)
+        }
+        index = index + 4
+    };
+
+
+    svgContainer.appendChild(gContainer)
+
+    return svgContainer
+  }
 
 }
